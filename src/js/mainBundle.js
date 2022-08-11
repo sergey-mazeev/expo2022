e926b5618aac17fe00de56656fcacd4384afbff3
Sergey Mazeev <sergey.mazeev@gmail.com>
2022-08-11 13:07:54 +0500

    initial commit

--- src/js/mainBundle.js
+++ src/js/mainBundle.js
@@ -94,6 +94,112 @@ const bindGallerySlider = (node) => {
     swiper.navigation.init();
 }
 
+const getPlanData = (path) => {
+    return fetch(path).then(data => data.json());
+}
+
+const closePlanDesc = (removePosition, changeActive) => {
+    document.querySelector('.plan__img-description').classList.remove('plan__img-description_open');
+
+    if (removePosition) {
+        document.querySelector('.plan__img-description').classList.remove('plan__img-description_left');
+        document.querySelector('.plan__img-description').classList.remove('plan__img-description_right');
+    }
+    if (changeActive) {
+        document.querySelector('.plan__list-item-active').classList.remove('plan__list-item-active');
+        document.querySelector('.plan-item-highlight').classList.remove('plan-item-highlight');
+    }
+}
+
+const openPlanDesc = () => {
+    document.querySelector('.plan__img-description').classList.add('plan__img-description_open');
+}
+
+const handlePlanClick = (link, data) => {
+    const id = link.getAttribute('data-item-id');
+    const position = link.getAttribute('data-desc-position');
+    data.then(({items}) => {
+        const item = items[id];
+
+        if (!item) {
+            return false;
+        }
+        const planList = document.querySelector('.plan__list');
+        planList.querySelector('.plan__list-item-active')
+        && planList.querySelector('.plan__list-item-active').classList.remove('plan__list-item-active');
+
+        const currentActive = document.querySelector('.plan-item-highlight');
+        if (currentActive === link) {
+            closePlanDesc(true);
+            link.classList.remove('plan-item-highlight');
+            return true;
+        }
+        if (currentActive) {
+            currentActive.classList.remove('plan-item-highlight');
+        }
+        planList.querySelector('[data-item-id="' + id + '"]').classList.add('plan__list-item-active')
+        link.classList.add('plan-item-highlight');
+
+        const change = fillPlanDescription(item, position);
+        if (change) {
+            closePlanDesc(false);
+            openPlanDesc();
+            return true;
+        }
+        openPlanDesc();
+    })
+}
+
+const bindPlanClick = (link, data) => {
+
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        handlePlanClick(link, data);
+    })
+}
+
+const fillPlanDescription = ({link, name, img, text}, position) => {
+
+    const desc = document.querySelector('.plan-desc');
+    const imageNode = desc.querySelector('.plan-desc__img-wrapper img');
+    const nameNode = desc.querySelector('.plan-desc__name a');
+    const textNode = desc.querySelector('.plan-desc__text');
+
+    imageNode.setAttribute('src', img);
+    imageNode.setAttribute('alt', name);
+    nameNode.setAttribute('href', link)
+    nameNode.innerText = name;
+    textNode.innerText = text;
+
+    const change = !desc.classList.contains(`plan__img-description_${position}`);
+    if (change) {
+        desc.classList.remove(`plan__img-description_${position === 'left' ? 'right' : 'left'}`);
+        desc.classList.add(`plan__img-description_${position}`);
+    }
+    return change;
+}
+
+const bindPlanListClick = (item, data) => {
+    if (!item || !data) {
+        return false;
+    }
+
+    item.addEventListener('click', () => {
+        const id = item.getAttribute('data-item-id');
+
+        if (!id) {
+            return false;
+        }
+
+        const target = document.querySelector('.plan__img-mask [data-item-id="' + id + '"]');
+        if (!target) {
+            return false;
+        }
+
+        handlePlanClick(target, data);
+    })
+}
+
 window.addEventListener('load', () => {
 
 
@@ -110,6 +216,20 @@ window.addEventListener('load', () => {
     const ctoSliders = document.querySelectorAll('.cto__gallery .swiper');
     for (const slider of ctoSliders) {
         bindCtoSlider(slider);
+    }
+
+    const planData = getPlanData('/plan-data.json');
+    for (const planItem of document.querySelectorAll('.js-plan-item')) {
+        bindPlanClick(planItem, planData)
+    }
+
+    document.querySelector('.plan-desc__close').addEventListener('click', (e) => {
+        e.preventDefault();
+        closePlanDesc(true, true);
+    })
+
+    for (const planListItem of document.querySelectorAll('.js-plan-list-item')) {
+        bindPlanListClick(planListItem, planData);
     }
 
 
@@ -151,7 +271,6 @@ window.addEventListener('load', () => {
         })
     }
     frontTabs(document.querySelector('.front-tabs'));
-
 
 
     const bindBurger = (node) => {
@@ -206,7 +325,6 @@ window.addEventListener('load', () => {
     expandBlock('.btn_icon-expand', '.about-goods__list-mobile');
 
 
-
     // инициализация кастомных модалок
     const on = (element, eventName, selector, fn) => {
         if (typeof element !== 'undefined' && element) {
@@ -245,19 +363,18 @@ window.addEventListener('load', () => {
     }
 
 
-
     const elemInViewport = (elem, full) => {
         const box = elem.getBoundingClientRect(),
-        top = box.top,
-        left = box.left,
-        bottom = box.bottom,
-        right  = box.right,
-        width = document.documentElement.clientWidth,
-        height = document.documentElement.clientHeight,
-        maxWidth = full ? right - left : 0,
-        maxHeight = full ? bottom - top : 0;
+            top = box.top,
+            left = box.left,
+            bottom = box.bottom,
+            right = box.right,
+            width = document.documentElement.clientWidth,
+            height = document.documentElement.clientHeight,
+            maxWidth = full ? right - left : 0,
+            maxHeight = full ? bottom - top : 0;
 
-        return Math.min(height, bottom)- Math.max(0,top) >= maxHeight && Math.min(width, right)- Math.max(0, left)>= maxWidth;
+        return Math.min(height, bottom) - Math.max(0, top) >= maxHeight && Math.min(width, right) - Math.max(0, left) >= maxWidth;
     }
 
     const animateImages = () => {
